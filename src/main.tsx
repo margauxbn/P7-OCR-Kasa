8ee6aea7402b4d7d83f997b0de5424d33742a457
Margaux Burban <burban.margaux@gmail.com>
2024-07-25 11:11:55 +0200

    Création du routeur et des différents paths dans main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import "./style.scss"
+import "./style.scss";
+import { createBrowserRouter, RouterProvider, } from "react-router-dom";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <div>404 error</div> // Joindre le fichier 404
+  },
+  {
+    path: "/location",
+    element: <div>Cozy loft</div> 
+  },
+  {
+    path: "/about",
+    element: <div>À propos</div>
+  },
+]);
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
